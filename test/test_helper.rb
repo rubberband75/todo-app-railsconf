c426f9622fd28791bc098fe9179968dbff541d89
Alex Wood <awood45@gmail.com>
2015-04-09 15:35:44 -0700

    Todo App Functionality
    The app functions, but does not have any AWS integrations included.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,22 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'capybara/rails'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+end
 
-  # Add more helper methods to be used by all tests here...
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
+  ENV['FROM_EMAIL'] = 'from@example.com'
+
+  def login_as_test_user
+    visit root_path
+    click_link 'Log In'
+    visit '/login'
+    fill_in 'Email',    with: 'test@example.org'
+    fill_in 'Password', with: 'password'
+    click_button 'Log In'
+  end
 end
